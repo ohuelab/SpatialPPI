c848ec0e449048d145f0411dae7282daa2c7f57e
perseids2032 <62238775+perseids2032@users.noreply.github.com>
2024-01-21 13:42:38 +0900

    Update structure.py

--- utils/structure.py
+++ utils/structure.py
@@ -4,34 +4,24 @@ from .densenet3d import DenseNet3DImageNet121, DenseNet3DPPI, DenseNet3D
 
 def getModel(modelName, inputsize):
     if modelName == 'Resnet3D':
-        resnet3D = Resnet3DBuilder.build_resnet_18(inputsize, 2)
+        resnet3D = Resnet3DBuilder.build_resnet_50(inputsize, 2)
         return resnet3D
 
     elif modelName == 'DenseNet3D':
-        return DenseNet3DImageNet121(input_shape=inputsize,
-                                     bottleneck=True,
-                                     reduction=0.5,
-                                     dropout_rate=0.2,
-                                     weight_decay=1e-4,
-                                     include_top=True,
-                                     input_tensor=None,
-                                     pooling='max',
-                                     classes=2,
-                                     activation='softmax')
-    elif modelName == 'DenseNet3DPPI':
-        return DenseNet3DPPI(input_shape=inputsize,
-                             bottleneck=True,
-                             reduction=0.5,
-                             dropout_rate=0.2,
-                             weight_decay=1e-4,
-                             include_top=True,
-                             input_tensor=None,
-                             pooling='max',
-                             classes=2,
-                             activation='softmax')
-    
-    return DenseNet3D(
-        inputsize,
-        classes=2,
-        activation='softmax'
-    )
+        return DenseNet3D(
+            inputsize,
+            nb_dense_block=4,
+            growth_rate=16,
+            nb_filter=32,
+            nb_layers_per_block=[4, 4, 4, 4],
+            bottleneck=True,
+            reduction=0.5,
+            dropout_rate=0.2,
+            weight_decay=1e-4,
+            subsample_initial_block=True,
+            include_top=True,
+            input_tensor=None,
+            pooling='max',
+            classes=2,
+            activation='softmax'
+        )
